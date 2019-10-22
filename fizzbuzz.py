60ef24443f361ebc8ae9d425e9429fd065a8c669
Michael Kopp-Jones <m.koppjones@gmail.com>
2019-10-22 13:32:43 +0100

    Add rule checking

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,49 @@
-rules = {
-  'red': 4,
-  'yellow': 7
-}
-  
-def parseOutput(number, ruleDict):
-  out = ''
-  for name, value in ruleDict.items():
-    if i % value == 0:
-      out += name
-  return out
+import sys
+import json
+import argparse
 
-print('Over engineered fizz buzz')
-print('Rules')
-print(rules)
-for i in range(1, 101):
-  out = parseOutput(i, rules)
-  if out.__len__() == 0:
-    out = i
-  print(out)
+# Set up command line arguments
+parser = argparse.ArgumentParser(description="Run customisable fizz buzz")
+parser.add_argument(
+    "rules", type=str, help="a json string defining the fizz buzz rules"
+)
+parser.add_argument(
+    "--min", type=int, default=1, help="optional minimum value, defaults to 1"
+)
+parser.add_argument(
+    "--max", type=int, default=100, help="optional maximum value, defaults to 100"
+)
+
+args = parser.parse_args()
+
+rulesDict = json.loads(args.rules)
+
+
+def parseOutput(number, ruleDict):
+    out = ""
+    for name, value in ruleDict.items():
+        if number % value == 0:
+            out += name
+    return out
+
+
+def checkRules(rules):
+    # Make sure that each key in the rule dictionary has an integer as a value
+    for key, value in rules.items():
+        if (not isinstance(value, int)):
+            raise ValueError(f'the key "{key}" in the rules does not have a valid')
+    return True
+
+
+def fizzBuzzRunner(rules, min, max):
+    try:
+        checkRules(rules)
+        for i in range(min, max + 1):
+            out = parseOutput(i, rules)
+            if out.__len__() == 0:
+                out = i
+            print(out)
+    except ValueError as ve:
+        print(ve)
+
+fizzBuzzRunner(rulesDict, args.min, args.max)
